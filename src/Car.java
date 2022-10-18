e30592e373f7f519a0493344ce6868df370be7ba
User <anton_trenogin@mail.ru>
2022-10-18 15:46:20 +0500

    Задача 3

---
+++ src/Car.java
@@ -0,0 +1,76 @@
+public class Car {
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int productionYear;
+    private String productionCountry;
+
+    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionCountry = productionCountry;
+        this.productionYear = productionYear;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    public void setProductionCountry(String productionCountry) {
+        this.productionCountry = productionCountry;
+    }
+
+    @Override
+    public String toString() {
+        return "Автомобиль: " + '\n' +
+                "Марка: " + brand +
+                ", Модель: " + model +
+                ", Объем двигателя(в литрах): " + engineVolume +
+                ", цвет: " + color +
+                ", год производства: " + productionYear +
+                ", страна: " + productionCountry;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car granta = new Car("Lada", "Granta", 1.7, "жёлтый", 2015, "Россия");
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0, "чёрный", 2020, "Германия");
+        Car bmw = new Car("BMW", "Z8", 3.0, "чёрный", 2021, "Германия");
+        Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
+        Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+
+        System.out.println(granta);
+        System.out.println(audi);
+        System.out.println(bmw);
+        System.out.println(kia);
+        System.out.println(hyundai);
     }
 }
